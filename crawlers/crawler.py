885b0021e24d914ac09062d764b3bb1ee80dc568
haandol <ldg55d@gmail.com>
2019-07-18 19:26:01 +0900

    add crawler for gsshop

--- crawlers/crawler.py
+++ crawlers/crawler.py
@@ -1,10 +1,65 @@
 import re
+import boto3
 import requests
+from decimal import Decimal
 from datetime import datetime
 from bs4 import BeautifulSoup
 
-class Agent:
+
+class Product:
+    def __init__(self, data):
+        self.id = data['id']
+        self.shop = data['shop']
+        self.from_at = self._refine_times(data['from_at'])
+        self.to_at = self._refine_times(data['to_at'])
+        self.img = data['img']
+        self.name = data['name']
+        self.link = data['link']
+        self.price = self._refine_price(data['price'])
+
+    def _refine_times(self, times):
+        if isinstance(times, Decimal):
+            return times
+
+        return Decimal(times.replace(':', ''))
+
+    def _refine_price(self, price):
+        if isinstance(price, Decimal):
+            return price
+
+        if not price:
+            return 0
+        
+        text = price[0].get_text().replace(',', '')
+        return Decimal(re.search(r'(\d+)', text).group(0))
+
+    def to_item(self):
+        return {
+            'id': self.id, 
+            'shop': self.shop,
+            'from_at': self.from_at, 
+            'to_at': self.to_at, 
+            'img': self.img, 
+            'name': self.name, 
+            'link': self.link, 
+            'price': self.price, 
+        }
+
+    def __eq__(self, other):
+        if not isinstance(other, Product):
+            return False
+        
+        fields = ['id', 'from_at', 'to_at', 'price', 'name', 'link', 'img']
+        for field in fields:
+            if getattr(self, field) != getattr(other, field):
+                print(getattr(self, field),  getattr(other, field))
+                return False
+        return True
+ 
+
+class GSAgent:
     def __init__(self):
+        self.shop = 'gs'
         self.headers = {
             'user-agent': 'Mozilla/5.0 Chrome/75.0.3770.142 Safari/537.36'
         }
@@ -15,51 +70,100 @@ class Agent:
         res = requests.get(self.URL + today, headers=self.headers)
         return res.content
 
-    def _refine_times(self, times):
-        return list(map(
-            lambda x: list(map(int, x.strip().split(':'))),
-            times
-        ))
-
-    def _refine_price(self, price):
-        if not price:
-            return 0
-        
-        text = price[0].get_text().replace(',', '')
-        return int(re.search(r'(\d+)', text).group(0))
-
     def _parse(self, content):
-        result = []
+        products = []
         soup = BeautifulSoup(content, 'html.parser')
         items = soup.find_all('article', 'items')
         for item in items:
-            product = {}
-            times = self._refine_times(item.select('.times')[0].get_text().split('-'))
             item_id = item.select('li.prd-item')[0]['id'].split('_')[1]
+            times = item.select('.times')[0].get_text().split('-')
             img = 'http:' + item.select('img')[0]['src']
             info = item.select('dl.prd-info')[0]
             name = info.select('.prd-name a')[0]
             link = self.BASE_URL + name['href']
             price = info.select('.price-info strong')
-            product = {
-                'time': {
-                    'from': times[0],
-                    'to': times[1]
-                },
-                'id': item_id,
+
+            products.append(Product({
+                'id': '{}-{}'.format(self.shop, item_id),
+                'shop': self.shop,
+                'from_at': times[0].strip(),
+                'to_at': times[1].strip(),
                 'img': img,
-                'name': name.get_text(),
+                'name': name.get_text().strip(),
                 'link': link,
-                'price': self._refine_price(price)
-            }
-            result.append(product)
-        return result
+                'price': price,
+            }))
+        return products
 
     def crawl(self):
         today = datetime.now().strftime('%Y%m%d') 
-        data = self._fetch(today)
-        return self._parse(data)
+        response = self._fetch(today)
+        return self._parse(response)
+
+    def update_products(self, db, products):
+        table = db.get_or_create_table('Product')
+        updated_products = []
+        for product in products:
+            old = table.get_item(
+                Key={
+                    'id': product.id,
+                    'shop': product.shop,
+                }
+            )
+            if (Product(old['Item']) != product):
+                table.put_item(
+                    Item=product.to_item(),
+                )
+                updated_products.append(product.id)
+
+        return updated_products
 
 
-agent = Agent()
-print(agent.crawl())
+class DynamoDB:
+    def __init__(self):
+        self._resouce = None
+
+    @property
+    def resouce(self):
+        if self._resouce:
+            return self._resouce
+
+        self._resouce = boto3.resource('dynamodb',
+                                      region_name='ap-northeast-2',
+                                      endpoint_url='http://apseonote260:8000')
+        return self._resouce
+
+    def get_or_create_table(self, table_name):
+        try:
+            params = dict(
+                TableName=table_name,
+                KeySchema=[
+                    {'AttributeName': 'id', 'KeyType': 'HASH'},
+                    {'AttributeName': 'shop', 'KeyType': 'RANGE'},
+                ],
+                AttributeDefinitions=[
+                    {'AttributeName': 'id', 'AttributeType': 'S'},
+                    {'AttributeName': 'shop', 'AttributeType': 'S'},
+                ],
+                ProvisionedThroughput={
+                    'ReadCapacityUnits': 10,
+                    'WriteCapacityUnits': 10,
+                },
+            )
+            client = boto3.client('dynamodb',
+                                  region_name='ap-northeast-2',
+                                  endpoint_url='http://apseonote260:8000')
+            client.create_table(**params)
+            waiter = client.get_watier('table_exists')
+            waiter.wait(TableName=table_name)
+        except Exception as e:
+            return self.resouce.Table(table_name)
+
+
+if __name__ == '__main__':
+    agent = GSAgent()
+    products = agent.crawl()
+    db = DynamoDB()
+    updated_product_ids = agent.update_products(db, products)
+    if updated_product_ids:
+        print(len(updated_product_ids))
