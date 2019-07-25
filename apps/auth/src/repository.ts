b986df6f063ee2bcdef7a44c7975a1b45e03acf5
haandol <ldg55d@gmail.com>
2019-07-26 01:24:59 +0900

    apply pooling to pg connection

--- apps/auth/src/repository.ts
+++ apps/auth/src/repository.ts
@@ -1,4 +1,4 @@
-import * as pg from 'pg';
+import { Pool } from 'pg';
 import { Account, UserToken } from './interface';
 import config from './config';
 import logger from './logger';
@@ -7,70 +7,78 @@ const HOST = config.PG_HOST;
 const PORT = config.PG_PORT;
 
 class Repository {
-  client: pg.Client;
+  pool: Pool;
 
   constructor() {
-    this.client = new pg.Client({
+    this.pool = new Pool({
       host: HOST,
       port: Number(PORT),
       database: 'authdb',
       user: 'master',
       password: 'aksekfls123',
-    });
-    this.initDB().catch((e) => {
-      logger.error(e);
-      throw e;
-    });
-  }
-
-  async initDB() {
-    await this.client.connect();
+      connectionTimeoutMillis: 5000,
+    })
   }
 
   async createAccount(email: string, password: string): Promise<Account> {
-    const res = await this.client.query({
-      text: 'INSERT INTO users(email, password) VALUES($1, $2) RETURNING *',
-      values: [email, password]
-    });
-    logger.debug(`INSERT: ${JSON.stringify(res.rows)}`);
-    return {
-      email: res.rows[0].email,
-      password: res.rows[0].password,
-      accessToken: res.rows[0].access_token,
-      refreshToken: res.rows[0].refresh_token,
-    };
-  }
-
-  async getAccountByEmail(email: string): Promise<Account | null> {
-    const res = await this.client.query({
-      text: 'SELECT * FROM users WHERE email = $1',
-      values: [email],
-    });
-    logger.debug(`SELECT: ${JSON.stringify(res.rows)}`);
-
-    if (res.rows && res.rows[0]) {
+    const client = await this.pool.connect();
+    try {
+      const res = await client.query({
+        text: 'INSERT INTO users(email, password) VALUES($1, $2) RETURNING *',
+        values: [email, password]
+      });
+      logger.debug(`INSERT: ${JSON.stringify(res.rows)}`);
       return {
         email: res.rows[0].email,
         password: res.rows[0].password,
         accessToken: res.rows[0].access_token,
         refreshToken: res.rows[0].refresh_token,
       };
-    } else {
-      return null;
+    } finally {
+      client.release();
+    }
+ }
+
+  async getAccountByEmail(email: string): Promise<Account | null> {
+    const client = await this.pool.connect();
+    try {
+      const res = await client.query({
+        text: 'SELECT * FROM users WHERE email = $1',
+        values: [email],
+      });
+      logger.debug(`SELECT: ${JSON.stringify(res.rows)}`);
+
+      if (res.rows && res.rows[0]) {
+        return {
+          email: res.rows[0].email,
+          password: res.rows[0].password,
+          accessToken: res.rows[0].access_token,
+          refreshToken: res.rows[0].refresh_token,
+        };
+      } else {
+        return null;
+      }
+    } finally {
+      client.release();
     }
   }
 
   async updateToken(email: string, accessToken: string, refreshToken: string): Promise<UserToken> {
-    const res = await this.client.query({
-      text: 'UPDATE users SET access_token = $1, refresh_token = $2 WHERE email = $3 RETURNING email, access_token, refresh_token',
-      values: [accessToken, refreshToken, email],
-    });
-    logger.debug(`UPDATE: ${JSON.stringify(res.rows)}`);
-    return {
-      email: res.rows[0].email,
-      accessToken: res.rows[0].access_token,
-      refreshToken: res.rows[0].refresh_token,
-    };
+    const client = await this.pool.connect();
+    try {
+      const res = await client.query({
+        text: 'UPDATE users SET access_token = $1, refresh_token = $2 WHERE email = $3 RETURNING email, access_token, refresh_token',
+        values: [accessToken, refreshToken, email],
+      });
+      logger.debug(`UPDATE: ${JSON.stringify(res.rows)}`);
+      return {
+        email: res.rows[0].email,
+        accessToken: res.rows[0].access_token,
+        refreshToken: res.rows[0].refresh_token,
+      };
+    } finally {
+      client.release();
+    }
   }
 }
 
