201484db6aeb9793d3522fcc766fa46f510e23f8
haandol <ldg55d@gmail.com>
2019-07-25 13:21:32 +0900

    remove security group for elasticache

--- terraform/elasticache.tf
+++ terraform/elasticache.tf
@@ -1,40 +1,11 @@
 resource "aws_elasticache_cluster" "redis_cluster" {
-  cluster_id = "reids_cluster"  
+  cluster_id = "reids"  
   engine = "redis"
-  engine_version = "3.2.4"
+  engine_version = "5.0.4"
   node_type = "cache.t2.micro"
   num_cache_nodes = 1
-  parameter_group_name = "${aws_elasticache_parameter_group.default.name}"
-  security_group_names = ["${aws_elasticache_security_group.redis.name}"]
+  parameter_group_name = "default.redis5.0"
   port = 6379
-}
-
-resource "aws_elasticache_parameter_group" "default" {
-  name = "redis-params"
-  family = "redis3.2"
-}
-
-resource "aws_security_group" "redis" {
-  name = "redis"
-
-  ingress {
-    from_port= 5379
-    to_port = 5379
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_elasticache_security_group" "redis" {
-  name = "elasticache-security-group"
-  security_group_names = ["${aws_security_group.redis.name}"]
 }
 
 output "redis_public_address" {
