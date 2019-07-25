09d80b932cc5bb8650a4b6a0ee7e593823d8bcf5
Lee <dongklee@ea.com>
2019-07-25 17:55:21 +0900

    [Bugfix] resolve infinity loop

--- web/pages/signin.tsx
+++ web/pages/signin.tsx
@@ -1,9 +1,95 @@
+import React from 'react';
+import axios from 'axios';
+import config from '../config';
 import Layout from '../components/Layout';
 
-const Signin = () => (
-  <Layout>
-    <Signin />
-  </Layout>
-);
+interface IProps {
+}
+
+interface IState {
+  email: string;
+  password: string;
+}
+
+class Signin extends React.Component<IProps, IState> {
+  private _submitSignin: (e: any) => Promise<void>;
+
+  constructor(props: IProps) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: '',
+    }
+
+    this._submitSignin = async (e:React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+
+      const { email, password } = this.state;
+      if (!email) {
+        alert('No Email');
+        return;
+      } else if (!password) {
+        alert('No Password');
+        return;
+      }
+
+      try {
+        const res = await axios({
+          method: 'post',
+          url: config.AUTH_URL + '/signin',
+          data: {
+            email: email,
+            password: password,
+          },
+          timeout: 3000,
+          responseType: 'json',
+        });
+
+        console.log(res);
+        const accessToken = res && res.data && res.data.access_token;
+        if (!accessToken) {
+          alert('No access token');
+        } else {
+          localStorage.setItem('token', accessToken);
+        }
+      } catch(e) {
+        console.error(e);
+        alert('Failed to get token');
+      }
+   }
+  }
+
+  onChangeEmail(email: string) {
+    this.setState({ email });
+  }
+
+  onChangePassword(password: string) {
+    this.setState({ password });
+  }
+
+  render() {
+    const { email, password } = this.state;
+
+    return (
+      <Layout>
+        <div className="signin-form">
+          <h1>Signin</h1>
+          <form onSubmit={this._submitSignin}>
+            <label>
+              Email:
+              <input type="text" placeholder="email" value={email} onChange={e => this.onChangeEmail(e.target.value)} />
+            </label>
+            <label>
+              Password:
+              <input type="password" value={password} onChange={e => this.onChangePassword(e.target.value)} />
+            </label>
+            <input type="submit" value="Signin" />
+          </form>
+        </div>
+      </Layout>
+    );
+  }
+}
 
 export default Signin;
