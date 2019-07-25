13488b39763c9fdafaac48b42eebbd7111c02120
Lee <dongklee@ea.com>
2019-07-25 18:44:38 +0900

    modify web urls

--- web/config.ts
+++ web/config.ts
@@ -1,4 +1,4 @@
 export default {
-  AUTH_URL: process.env.AUTH_URL || 'http://localhost:3000',
-  API_URL: process.env.API_URL || 'http://localhost:3001',
+  AUTH_URL: process.env.AUTH_URL || 'http://localhost:8000',
+  API_URL: process.env.API_URL || 'http://localhost:8001',
 }
