adeb29aaeac22cda6162356c40c448aa4880dda2
Lee <dongklee@ea.com>
2019-07-26 16:07:54 +0900

    add ecr login before push image to ecr

--- scripts/build_apps.sh
+++ scripts/build_apps.sh
@@ -19,6 +19,7 @@ echo GRAPHQL_API_URIS: $GQL_URIS
 REDIS=$(echo $LINES | jq '.redis_public_address.value' | tr -d '"')
 echo REDIS: $REDIS
 
+# Build
 cd ../apps
 APPS="auth product"
 for APP in $APPS; do
@@ -30,7 +31,13 @@ for APP in $APPS; do
 --build-arg RDS=$RDS \
 --build-arg REDIS=$REDIS \
 --no-cache .
-    cd ..
+done;
+
+# ECR
+aws ecr get-login --no-include-email --region ap-northeast-2
+for APP in $APPS; do
+    docker tag $APP:latest 348028092597.dkr.ecr.ap-northeast-2.amazonaws.com/$APP:latest
+    docker push 348028092597.dkr.ecr.ap-northeast-2.amazonaws.com/$APP:latest
 done;
 
 popd
