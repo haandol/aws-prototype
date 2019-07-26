3867b151f18a6e5d3b24f9d04b2be8eda58e2136
haandol <ldg55d@gmail.com>
2019-07-26 14:14:18 +0900

    add dockerfile for web

--- apps/product/src/config.ts
+++ apps/product/src/config.ts
@@ -1,8 +1,8 @@
 export default {
   aws_project_region: 'ap-northeast-2',
-  aws_appsync_graphqlEndpoint: process.env.AWS_APPSYNC_GRAPHQL_ENDPOINT || 'https://da25g5znkvgf5bolbdijdqj6p4.appsync-api.ap-northeast-2.amazonaws.com/graphql',
+  aws_appsync_graphqlEndpoint: process.env.AWS_APPSYNC_GRAPHQL_ENDPOINT || '',
   aws_appsync_authenticationType: 'API_KEY',
-  aws_appsync_apiKey: process.env.AWS_APPSYNC_APIKEY || 'da2-libpmlqtzvebbm6h7yazatd3zi',
+  aws_appsync_apiKey: process.env.AWS_APPSYNC_APIKEY || '',
   CLIENT_ID: process.env.CLIENT_ID || 'clientId',
   SECRET_KEY: process.env.SECRET_KEY || 'secretKey',
 }
