f92f25d4b238f8526eaf7e2fee77c348e105205b
Lee <dongklee@ea.com>
2019-07-31 16:13:12 +0900

    [Bugfix] modify build app script

--- scripts/build_apps.sh
+++ scripts/build_apps.sh
@@ -23,21 +23,22 @@ echo REDIS: $REDIS
 cd ../apps
 APPS="auth product"
 for APP in $APPS; do
-    cd $APP
-    echo Build $APP..
-    docker build -t $APP \
+  cd $APP
+  echo Build $APP..
+  docker build -t $APP:latest \
 --build-arg GQL_API_KEY=$GQL_API_KEY \
 --build-arg GQL_ENDPOINT=$GQL_ENDPOINT \
 --build-arg RDS=$RDS \
 --build-arg REDIS=$REDIS \
 --no-cache .
+  cd ..
 done;
 
 # ECR
 aws ecr get-login --no-include-email --region ap-northeast-2
 for APP in $APPS; do
-    docker tag $APP:latest 348028092597.dkr.ecr.ap-northeast-2.amazonaws.com/$APP:latest
-    docker push 348028092597.dkr.ecr.ap-northeast-2.amazonaws.com/$APP:latest
+  docker tag $APP:latest 348028092597.dkr.ecr.ap-northeast-2.amazonaws.com/$APP:latest
+  docker push 348028092597.dkr.ecr.ap-northeast-2.amazonaws.com/$APP:latest
 done;
 
 popd
