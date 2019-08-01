ddf09e8129cc65584eef9e163dff7d3f8b06a14e
Lee <dongklee@ea.com>
2019-08-01 11:05:01 +0900

    fix ecr login

--- scripts/build_web.sh
+++ scripts/build_web.sh
@@ -11,7 +11,8 @@ echo Build web..
 docker build -t web:latest --no-cache .
 
 # ECR
-aws ecr get-login --no-include-email --region ap-northeast-2
+eval $(aws ecr get-login --no-include-email --region ap-northeast-2)
+
 docker tag web:latest 348028092597.dkr.ecr.ap-northeast-2.amazonaws.com/web:latest
 docker push 348028092597.dkr.ecr.ap-northeast-2.amazonaws.com/web:latest
 
