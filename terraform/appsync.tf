9177bcb96310b32a1266746263af047c7d03783c
Lee <dongklee@ea.com>
2019-08-01 21:09:48 +0900

    modify limit default to 50

--- terraform/appsync.tf
+++ terraform/appsync.tf
@@ -206,7 +206,7 @@ resource "aws_appsync_resolver" "list_products" {
   "version": "2017-02-28",
   "operation": "Scan",
   "filter": #if($context.args.filter) $util.transform.toDynamoDBFilterExpression($ctx.args.filter) #else null #end,
-  "limit": $util.defaultIfNull($ctx.args.limit, 20),
+  "limit": $util.defaultIfNull($ctx.args.limit, 50),
   "nextToken": $util.toJson($util.defaultIfNullOrEmpty($ctx.args.nextToken, null)),
 }
 EOF
