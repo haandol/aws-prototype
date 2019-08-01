6fef610216183ad7ff5571ddb1ad1c2eb762f60c
Lee <dongklee@ea.com>
2019-08-01 14:08:08 +0900

    add dotenv for web

--- web/config.ts
+++ web/config.ts
@@ -1,3 +1,6 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 export default {
   AUTH_URL: process.env.AUTH_URL || 'http://localhost:8001',
   PRODUCT_URL: process.env.PRODUCT_URL || 'http://localhost:8000',
