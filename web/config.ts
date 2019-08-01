92a01a48c20657640d909b84da830f58ffa7803f
Lee <dongklee@ea.com>
2019-08-01 14:57:26 +0900

    add dotenv-webpack

--- web/config.ts
+++ web/config.ts
@@ -1,6 +1,3 @@
-import * as dotenv from 'dotenv';
-dotenv.config();
-
 export default {
   AUTH_URL: process.env.AUTH_URL || 'http://localhost:8001',
   PRODUCT_URL: process.env.PRODUCT_URL || 'http://localhost:8000',
